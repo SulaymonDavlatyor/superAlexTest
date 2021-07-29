6b5a385a6a9dddd3f19f7652c7414d81630eb249
SulaymonDavlatyor <76821700+SulaymonDavlatyor@users.noreply.github.com>
2021-07-29 15:54:44 +0500

    Update phpTest.php

--- phpTest.php
+++ phpTest.php
@@ -10,7 +10,7 @@ $mysqli  = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 
 $result_set = $mysqli->query("INSERT INTO users (tel, email, name, text, checkbox) 
-                                    VALUES (".$_POST['tel']." ".$_POST['email']." ".$_POST['name']." ".$_POST['text']." ".$_POST['checkbox'].")");
+                              VALUES (".$_POST['tel']." ".$_POST['email']." ".$_POST['name']." ".$_POST['text']." ".$_POST['checkbox'].")");
 
 $mysqli->close();
 
